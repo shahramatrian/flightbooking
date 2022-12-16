8a74fc89f8ddc8ec5122510693e911103875984b
Sean Atrian <seanatrian@promptlink.com>
2022-12-16 12:30:08 -0800

    Added simple exception handling

--- src/main/java/com/shahramatrian/flightbooking/controllers/BookingController.java
+++ src/main/java/com/shahramatrian/flightbooking/controllers/BookingController.java
@@ -1,5 +1,7 @@
 package com.shahramatrian.flightbooking.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,45 +11,65 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.shahramatrian.flightbooking.model.Booking;
 import com.shahramatrian.flightbooking.model.BookingStatus;
 import com.shahramatrian.flightbooking.services.BookingService;
 
+
 @RestController
-@RequestMapping("/api/v1/booking")
+@RequestMapping("/api/v1/bookings")
 public class BookingController {
+    private static final Logger Log = LoggerFactory.getLogger(BookingController.class);
+
     BookingService bookingService;
 
     public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
     }
 
-    private ResponseEntity<Booking> UpdateBookingStatus(Long bookingId, Booking booking, BookingStatus status) {
+    private ResponseEntity<Booking> updateBookingStatus(Long bookingId, Booking booking) {
         bookingService.updateBooking(bookingId, booking);
         return new ResponseEntity<>(bookingService.getBooking(bookingId), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Booking> reserveBooking(@RequestBody Booking booking) {
-        // TODO: check if booking is in correct status
         booking.setStatus(BookingStatus.UNCONFIRMED);
-        Booking newBooking = bookingService.createBooking(booking);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("booking", "/api/v1/booking/" + newBooking.getId().toString());
-        return new ResponseEntity<>(newBooking, httpHeaders, HttpStatus.CREATED);
+        try {
+            Booking newBooking = bookingService.createBooking(booking);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("booking", "/api/v1/booking/" + newBooking.getId().toString());
+            return new ResponseEntity<>(newBooking, httpHeaders, HttpStatus.CREATED);
+        } catch (Exception exc) {
+            Log.debug("Could not create booking");
+            throw new ResponseStatusException(
+                HttpStatus.CONFLICT, "Could not create booking", exc);
+        }
     }
 
     @PutMapping({"/confirm/{bookingId}"})
     public ResponseEntity<Booking> confirmBooking(@PathVariable("bookingId") Long bookingId, @RequestBody Booking booking) {
-        // TODO: check if booking status is UNCONFIRMED otherwise 
-        // throw an exception
-        return UpdateBookingStatus(bookingId, booking, BookingStatus.CONFIRMED);
+        if (booking.getStatus() == BookingStatus.UNCONFIRMED) {
+            booking.setStatus(BookingStatus.CONFIRMED);
+            return updateBookingStatus(bookingId, booking);
+        } else {
+            Log.debug("Could not confirm booking");
+            throw new ResponseStatusException(
+                HttpStatus.CONFLICT, "Could not confirm booking");
+        }
     }
 
     @PutMapping({"/cancel/{bookingId}"})
     public ResponseEntity<Booking> cancelBooking(@PathVariable("bookingId") Long bookingId, @RequestBody Booking booking) {
-        // TODO: check if booking is in correct status
-        return UpdateBookingStatus(bookingId, booking, BookingStatus.CANCELLED);
+        if (booking.getStatus() == BookingStatus.CONFIRMED || booking.getStatus() == BookingStatus.UNCONFIRMED) {
+            booking.setStatus(BookingStatus.CANCELLED);
+            return updateBookingStatus(bookingId, booking);
+        } else {
+            Log.debug("Could not cancel booking");
+            throw new ResponseStatusException(
+                HttpStatus.CONFLICT, "Could not cancel booking");
+        }
     }
 }
--- src/main/java/com/shahramatrian/flightbooking/controllers/FlightController.java
+++ src/main/java/com/shahramatrian/flightbooking/controllers/FlightController.java
@@ -2,6 +2,8 @@ package com.shahramatrian.flightbooking.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +14,15 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.shahramatrian.flightbooking.model.Flight;
 import com.shahramatrian.flightbooking.services.FlightService;
 
 @RestController
-@RequestMapping("/api/v1/flight")
+@RequestMapping("/api/v1/flights")
 public class FlightController {
+    private static final Logger Log = LoggerFactory.getLogger(FlightController.class);
     FlightService flightService;
 
     public FlightController(FlightService flightService) {
@@ -27,25 +31,41 @@ public class FlightController {
 
     @GetMapping
     public ResponseEntity<List<Flight>> getAllFlights() {
-        List<Flight> flights = flightService.listFlights();
-        return new ResponseEntity<>(flights, HttpStatus.OK);
+        try {
+            List<Flight> flights = flightService.listFlights();
+            return new ResponseEntity<>(flights, HttpStatus.OK);
+        } 
+        catch (Exception exc) {
+            Log.debug("Could not list flights");
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Could not list flights", exc);
+        }
     }
     
     @PostMapping
     public ResponseEntity<Flight> addFlight(@RequestBody Flight flight) {
-        Flight newFlight = flightService.createFlight(flight);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("flight", "/api/v1/flight/" + newFlight.getId().toString());
-        return new ResponseEntity<>(newFlight, httpHeaders, HttpStatus.CREATED);
+        try {
+            Flight newFlight = flightService.createFlight(flight);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("flight", "/api/v1/flight/" + newFlight.getId().toString());
+            return new ResponseEntity<>(newFlight, httpHeaders, HttpStatus.CREATED);
+        } catch (Exception exc) {
+            Log.debug("Could not add flight");
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT, "Could not add flight", exc);
+        }
     }
 
     @PutMapping({"/reserve/{flightId}"})
-    public ResponseEntity<Flight> reserveflight(@PathVariable("flightId") Long flightId) {
-        //TODO: cases where seat capacity is zero must be considered
+    public ResponseEntity<String> reserveflight(@PathVariable("flightId") Long flightId) {
         Flight flight = flightService.getFlightById(flightId);
-        flight.setSeatCapacity(flight.getSeatCapacity() - 1);
-        flightService.updateFlight(flightId, flight);
-        return new ResponseEntity<>(flightService.getFlightById(flightId), HttpStatus.OK);
+        if (flight.getSeatCapacity() > 0) {
+            flight.setSeatCapacity(flight.getSeatCapacity() - 1);
+            flightService.updateFlight(flightId, flight);
+            return new ResponseEntity<>("Flight reserved", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Flight not reserved. Seat capacity is zero", HttpStatus.OK);
+        }
     }
 
     @PutMapping({"/release/{flightId}"})
